da1c7da4cfb6756d305682802851b92d9ed7afed
xenik <xenikSeefka@gmail.com>
2013-04-17 15:53:45 +0400

    new colour of public and cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,4 +12,8 @@ class Cart < ActiveRecord::Base
   	end
   	current_item	
   end
+
+  def total_price
+    line_items.to_a.sum {|i| i.total_price}
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :item_id, :quantity
   belongs_to :cart
   belongs_to :item
+
+  def total_price
+  	item.price*quantity
+  end
 end
